cd03bb0a3e29eeb7dce2719a1262d7d5c4be2cc3
Reshma Shaik <kshaikreshma98uk@gmail.com>
2025-03-20 15:13:31 +0000

    fix: raise exception for negative numbers

--- calculator.js
+++ calculator.js
@@ -11,6 +11,12 @@ const add = (numbers) => {
   }
 
   const nums = numString.split(new RegExp(`${delimiter}|\\n`)).map(num => parseInt(num));
+
+  const negatives = nums.filter(num => num < 0);
+  if (negatives.length > 0) {
+    throw new Error(`negative numbers not allowed: ${negatives.join(", ")}`);
+  }
+
   return nums.reduce((sum, num) => sum + num, 0);
 }
 
