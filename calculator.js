69aae2b3802def03096e6a4449ff7779e5ae1923
Reshma Shaik <kshaikreshma98uk@gmail.com>
2025-03-20 15:07:40 +0000

    fix: add regex for newlines numbers

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 const add = (numbers) => {
   if (numbers === "") return 0;
-  const nums = numbers.split(",").map(num => parseInt(num));
+  const nums = numbers.split(/,|\n/).map(num => parseInt(num));
   return nums.reduce((sum, num) => sum + num, 0);
 }
 
