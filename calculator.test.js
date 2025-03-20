f264c89f8f58031bc2f8f9a2b468da7d4495eb21
Reshma Shaik <kshaikreshma98uk@gmail.com>
2025-03-20 14:22:51 +0000

    feat: handle single number test case

--- calculator.test.js
+++ calculator.test.js
@@ -3,4 +3,8 @@ describe("String Calculator", () => {
     const calculator = require("./calculator");
     expect(calculator.add("")).toBe(0);
   });
+
+  test("should return the number itself for a single number", () => {
+    expect(calculator.add("1")).toBe(1);
+  });
 });
