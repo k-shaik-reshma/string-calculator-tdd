cc707cf4616c347127230f6c78efcd5195b41d51
Reshma Shaik <kshaikreshma98uk@gmail.com>
2025-03-20 15:09:09 +0000

    fix: handle // delimiter in the numbers string

--- calculator.js
+++ calculator.js
@@ -1,6 +1,16 @@
 const add = (numbers) => {
   if (numbers === "") return 0;
-  const nums = numbers.split(/,|\n/).map(num => parseInt(num));
+  
+  let delimiter = ",";
+  let numberString = numbers;
+  
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n", 2);
+    delimiter = parts[0].slice(2);
+    numberString = parts[1];
+  }
+  
+  const nums = numberString.split(new RegExp(`${delimiter}|\\n`)).map(num => parseInt(num));
   return nums.reduce((sum, num) => sum + num, 0);
 }
 
